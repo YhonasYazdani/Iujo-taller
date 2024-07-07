00d702d96c5e0ae1bf44fac39f3d30931cdfc028
JoseVasquez1104 <profefarias80@gmail.com>
2024-07-07 16:30:26 -0400

    nombre de integrantes y enunciado

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/* Integrantes: Jonas Dominguez y Heracle Sanchez */
+
+/*Realice un programa que contenga un menú de opciones y en cada opción
+ejecutara los siguientes requerimientos.
+Un algoritmo que calcule el factorial de un número dado.
+Un algoritmo que calcule el valor absoluto de un número dato.
+Un algoritmo que calcule el promedio para cualquier cantidad de
+elementos dados.
+Se debe usar funciones
+*/
+
+int main(int argc, char** argv) {
+	return 0;
+}
